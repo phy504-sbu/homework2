74d0b844df1f9ee4705d01344b819004a4f2e7f5
Michael Belmonte <michael.belmonte@stonybrook.edu>
2022-02-14 15:00:19 -0500

    Finished prettyprint.sh to take -1 as an argument to print the full string.

--- prettyprint.sh
+++ prettyprint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 count=$2
+
 if [ $count -eq -1 ]
 then
-count=$(wc -c "${1}")
+  count=${#1}
 fi
-echo $count
+
 if [ $# -eq 2 ]
 then
   for i in $(seq 0 ${count})
@@ -12,5 +13,5 @@ then
     echo "${1:$i:${#1}}"
   done
 else
-  echo "Please include 2 arguments. For example: ./prettyprin.sh 'text' 1"
+  echo "Please include 2 arguments. For example: ./prettyprin.sh text 1"
 fi
